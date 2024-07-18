903b106d63c409de0f94d3f907d1f140c914100f
Jann Idar Hillestad <jihillestad@hotmail.com>
2024-07-18 21:17:32 +0200

    Stage 3: Site Header

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "JollyBlog",
+  url: "https://example.com",
+  description: "Nextjs 14 blog using velite, tailwind and shadcn",
+  author: "Jann Idar Hillestad",
+  links: {
+    twitter: "",
+    github: "https://github.com/Jihillestad",
+    personalSite: "",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
